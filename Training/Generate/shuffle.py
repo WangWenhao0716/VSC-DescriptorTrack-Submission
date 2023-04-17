4308451710889e348f4d071397d6ee0f2d12d8e0
Wenhao Wang <53076320+WangWenhao0716@users.noreply.github.com>
2023-04-18 01:03:54 +1000

    Update shuffle.py

--- Training/Generate/shuffle.py
+++ Training/Generate/shuffle.py
@@ -20,3 +20,19 @@ for i in range(len(r)):
     shutil.copytree(src, dst)
     
  
+ls_q = sorted(os.listdir('/raid/VSC/data/pull_gt_all_shuffle/gt_query/'))
+dic_q = dict()
+for i in range(len(ls_q)):
+    dic_q[ls_q[i]] = os.listdir('/raid/VSC/data/pull_gt_all_shuffle/gt_query/' + ls_q[i])[0]
+
+with open('pull_gt_all_shuffle_q.pickle', 'wb') as f:
+    pickle.dump(dic_q, f)
+
+ls_r = sorted(os.listdir('/raid/VSC/data/pull_gt_all_shuffle/gt_ref/'))
+dic_r = dict()
+for i in range(len(ls_r)):
+    dic_r[ls_r[i]] = os.listdir('/raid/VSC/data/pull_gt_all_shuffle/gt_ref/' + ls_r[i])[0]
+
+with open('pull_gt_all_shuffle_r.pickle', 'wb') as f:
+    pickle.dump(dic_r, f)
+    

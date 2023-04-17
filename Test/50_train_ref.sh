b0696409ec97a0f8e461e90cf73e30e4f4680417
Wenhao Wang <53076320+WangWenhao0716@users.noreply.github.com>
2023-04-18 04:24:10 +1000

    Update 50_train_ref.sh

--- Test/50_train_ref.sh
+++ Test/50_train_ref.sh
@@ -1,6 +1,6 @@
-mkdir -p ./feature/train_v1_50_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_2_cls_resize_FIN
+mkdir -p ./feature/train_v1_50_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_2_cls_FIN
 CUDA_VISIBLE_DEVICES=0 python extract_feature.py \
       --image_dir /raid/VSC/data/train/reference_one_second_imageio_v3 \
-      --o ./feature/train_v1_50_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_2_cls_resize_FIN/reference_v1.hdf5 \
+      --o ./feature/train_v1_50_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_2_cls_FIN/reference_v1.hdf5 \
       --model 50  --GeM_p 3 --bw \
-      --checkpoint train_v1_50_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_2_cls_resize_FIN.pth.tar --imsize 256 
+      --checkpoint train_v1_50_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_2_cls_FIN.pth.tar --imsize 256 

c134b5ce0c3930d37ba2c09085b1256b82cf4486
Wenhao Wang <53076320+WangWenhao0716@users.noreply.github.com>
2023-04-18 02:12:52 +1000

    Create clean_vit.py

---
+++ Training/Stage_23/clean_vit.py
@@ -0,0 +1,4 @@
+import torch
+mod = torch.load('logs/train_v1_s3_all_bw/vit_two_losses_com_L2_norm_100_all_tune_bw_gt_ng_1_cls_resize/checkpoint_9_ema.pth.tar',map_location='cpu')
+torch.save(mod['state_dict'], \
+'logs/train_v1_s3_all_bw/vit_two_losses_com_L2_norm_100_all_tune_bw_gt_ng_1_cls_resize/train_v1_vit_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_1_cls_resize_FIN.pth.tar')

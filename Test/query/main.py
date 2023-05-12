584fa22b1197b6e732b033d143a7d9af5445d091
Wenhao Wang <53076320+WangWenhao0716@users.noreply.github.com>
2023-05-13 03:00:23 +1000

    Update main.py

--- Test/query/main.py
+++ Test/query/main.py
@@ -26,10 +26,54 @@ QRY_VIDEOS_DIRECTORY = DATA_DIRECTORY + "query/"
 OUTPUT_FILE = ROOT_DIRECTORY + "subset_query_descriptors.npz"
 QUERY_SUBSET_FILE = DATA_DIRECTORY + "query_subset.csv"
 
+def videos_to_images_half(query_subset_video_ids):
+    path_save = '/dev/shm/query_half_second_ff_v2/'
+    os.makedirs(path_save, exist_ok = True)
+    ls = query_subset_video_ids
+    for i in range(len(ls)):
+        name = ls[i]
+        os.system('ffmpeg -hide_banner -loglevel panic -ss 00:00:00 -i ' + QRY_VIDEOS_DIRECTORY + name + '.mp4 -vf fps=2 -qscale:v 1 -start_number 0 ' + path_save + name + '_%d.jpg > /dev/null')
 
-def tranditional_filter():
-    os.system("conda run --no-capture-output -n condaenv python tranditional_filter.py")
-    #os.system("./tranditional_filter")
+def extract_feature_50SK_half():
+    os.makedirs('./feature/train_v1_50SK_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_2_cls_half/', exist_ok = True)
+    os.system('conda run --no-capture-output -n condaenv python extract_feature.py \
+      --image_dir /dev/shm/query_half_second_ff_v2 \
+      --o ./feature/train_v1_50SK_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_2_cls_half/query_v1_ff.hdf5 \
+      --model 50SK  --GeM_p 3 --bw \
+      --checkpoint train_v1_50SK_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_2_cls_FIN.pth.tar --imsize 256')
+
+def get_candidates():
+    path = './feature/train_v1_50SK_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_2_cls_half/'
+    ls = os.listdir(path)
+    q_name, q_feature = read_descriptors([path + 'query_%d_v1_ff.hdf5'%i for i in range(len(ls))])
+    q_name = np.array(q_name)
+    series = []
+    for q in q_name:
+        if '__' in q:
+            series.append(int(q.split('_')[0][1:])*1000000000 + int(q.split('_')[1])*1000 + int(q.split('__')[1]) + 1)
+        else:
+            series.append(int(q.split('_')[0][1:])*1000000000 + int(q.split('_')[1])*1000)
+    q_name_new = q_name[np.argsort(series)]
+    q_feature_new = q_feature[np.argsort(series)]
+    timestamps = np.array([[float(q.split('_')[1])*0.5, float(q.split('_')[1])*0.5+0.5] for q in q_name_new], dtype=np.float32)
+    path_to = path + 'query_v1_ff_sort.npz'
+    q_name_new_clean = [q.split('_')[0] for q in q_name_new]
+
+    features = q_feature_new
+    video_ids = np.array(q_name_new_clean)
+    timestamps = timestamps
+    all_id = sorted(list(set(list(video_ids))))
+    print("The length of all_id is", len(all_id))
+    save = []
+    for query_id in all_id:
+        q_matrix = features[video_ids==query_id]
+        ans = q_matrix@q_matrix.T
+        scores = np.diag(ans, k=1)
+        if min(scores)<0.3:
+            save.append(query_id)
+
+    with open('save_50SK_2_ff.pkl', 'wb') as f:
+        pickle.dump(save, f)
 
 def videos_to_images(query_subset_video_ids):
     path_save = '/dev/shm/query_one_second_imageio_v3/'
@@ -53,7 +97,7 @@ class PPad:
         color_3 = 255
         x = Pad(w_factor = w_factor, h_factor = h_factor, color = (color_1, color_2, color_3))(x)
         return x
-    
+
 def detectection():
     ls = sorted(os.listdir('/dev/shm/query_one_second_imageio_v3'))
     os.makedirs('/dev/shm/query_one_second_imageio_v3_detection_media_v3/', exist_ok = True)
@@ -65,7 +109,7 @@ def detectection():
             img = Image.open(src)
             img = PPad()(img)
             img.save(dst, quality=100)
-            
+
     os.makedirs("/dev/shm/query_one_second_imageio_v3_detection_v3", exist_ok = True)
     for i in range(len(ls)):
         name = ls[i]
@@ -76,7 +120,7 @@ def detectection():
     os.system('conda run --no-capture-output -n condaenv \
     python detect.py --source /dev/shm/query_one_second_imageio_v3_detection_media_v3 \
     --weights best_20230101.pt --conf 0.1 > /dev/null')
-    
+
 def generation():
     path = '/dev/shm/query_one_second_imageio_v3_detection_media_v3/'
     path_new = '/dev/shm/query_one_second_imageio_v3_detection_v3/'
@@ -106,10 +150,10 @@ def generation():
             dst = path_new + name
             shutil.copy(src, dst)
     assert len(os.listdir(path_new)) == len(os.listdir(path_ori))
-    
+
 def fix_rotate():
     os.system("conda run --no-capture-output -n condaenv python fix_rotate.py")
-            
+
 def extract_feature_vit():
     os.makedirs('./feature/train_v1_vit_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_1_cls_resize_FIN/', exist_ok = True)
     os.system('conda run --no-capture-output -n condaenv python extract_feature.py \
@@ -125,7 +169,7 @@ def extract_feature_swin():
       --o ./feature/train_v1_swin_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_1_cls_resize_FIN/query_v1_detection_rotate.hdf5 \
       --model swin_base  --GeM_p 3 --bw \
       --checkpoint train_v1_swin_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_1_cls_resize_FIN.pth.tar --imsize 224')
-    
+
 def extract_feature_t2t():
     os.makedirs('./feature/train_v1_t2t_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_1_cls_resize_FIN/', exist_ok = True)
     os.system('conda run --no-capture-output -n condaenv python extract_feature.py \
@@ -133,7 +177,7 @@ def extract_feature_t2t():
       --o ./feature/train_v1_t2t_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_1_cls_resize_FIN/query_v1_detection_rotate.hdf5 \
       --model t2t  --GeM_p 3 --bw \
       --checkpoint train_v1_t2t_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_1_cls_resize_FIN.pth.tar --imsize 224')
-    
+
 def extract_feature_50():
     os.makedirs('./feature/train_v1_50_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_2_cls_FIN/', exist_ok = True)
     os.system('conda run --no-capture-output -n condaenv python extract_feature.py \
@@ -156,35 +200,35 @@ def extract_feature_50X():
       --image_dir /dev/shm/query_one_second_imageio_v3_detection_v3_rotate_2 \
       --o ./feature/train_v1_50X_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_2_cls_FIN/query_v1_detection_rotate.hdf5 \
       --model 50X  --GeM_p 3 --bw \
-      --checkpoint train_v1_50X_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_2_cls_FIN.pth.tar --imsize 256')    
-    
+      --checkpoint train_v1_50X_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_2_cls_FIN.pth.tar --imsize 256')
+
 def ensemble():
     num = len(os.listdir("./feature/train_v1_50_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_2_cls_FIN"))
-    
+
     name, resnet =read_descriptors(
         ['./feature/train_v1_50_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_2_cls_FIN/query_%d_v1_detection_rotate.hdf5'%i for i in range(num)]
     )
-    
+
     name, resneXt =read_descriptors(
         ['./feature/train_v1_50X_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_2_cls_FIN/query_%d_v1_detection_rotate.hdf5'%i for i in range(num)]
     )
-    
+
     name, sknet =read_descriptors(
         ['./feature/train_v1_50SK_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_2_cls_FIN/query_%d_v1_detection_rotate.hdf5'%i for i in range(num)]
     )
-    
+
     name, vit =read_descriptors(
         ['./feature/train_v1_vit_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_1_cls_resize_FIN/query_%d_v1_detection_rotate.hdf5'%i for i in range(num)]
     )
-    
+
     name, swin =read_descriptors(
         ['./feature/train_v1_swin_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_1_cls_resize_FIN/query_%d_v1_detection_rotate.hdf5'%i for i in range(num)]
     )
-    
+
     name, t2t =read_descriptors(
         ['./feature/train_v1_t2t_s3_512_twolosses_com_L2_norm_100_all_tune_bw_gt_ng_1_cls_resize_FIN/query_%d_v1_detection_rotate.hdf5'%i for i in range(num)]
     )
-    
+
     select_features = []
     for i in range(len(name)):
         f_2 = resnet[i].reshape((512,1))
@@ -208,34 +252,37 @@ def score_normalization():
     --out_path ./ \
     --o ./predictions_train_subset.csv \
     --reduction avg --max_results 2000_000")
-    
+
 def main():
     import time
     # Loading subset of query images
     begin = time.time()
-    #query_subset = pd.read_csv(QUERY_SUBSET_FILE)
-    #query_subset_video_ids = query_subset.video_id.values.astype("U")
+    query_subset = pd.read_csv(QUERY_SUBSET_FILE)
+    query_subset_video_ids = query_subset.video_id.values.astype("U")
 
     print("Install packages")
-    #os.system("conda run --no-capture-output -n condaenv python -m pip install imageio_ffmpeg-0.4.8-py3-none-manylinux2010_x86_64.whl --force-reinstall")
+    os.system("conda run --no-capture-output -n condaenv python -m pip install imageio_ffmpeg-0.4.8-py3-none-manylinux2010_x86_64.whl --force-reinstall")
     os.system("conda run --no-capture-output -n condaenv python -m pip install timm-0.4.12-py3-none-any.whl")
-    #os.system("conda uninstall opencv -n condaenv -y")
-    #os.system("conda run --no-capture-output -n condaenv python -m pip install opencv_python-4.5.4.60-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl --no-deps --force-reinstall")
-    
-    print("Perform traditional filters!")
-    tranditional_filter()
-    with open('tranditional_filter.pkl', 'rb') as f:
-        query_subset_video_ids = pickle.load(f)
-    query_subset_video_ids = sorted(list(set(query_subset_video_ids)))
-    print(len(query_subset_video_ids))
+
+    print("Perform Deep Learning filter!")
+    videos_to_images_half(query_subset_video_ids)
+    extract_feature_50SK_half()
+    get_candidates()
 
     print("Transfer videos to images every one second using imageio")
-    videos_to_images(query_subset_video_ids)
-    
+    with open('save_50SK_2_ff.pkl', 'rb') as f:
+        query_subset_video_ids_filter_1 = pickle.load(f)
+
+    query_subset_video_ids_filter = query_subset_video_ids_filter_1
+    query_subset_video_ids_filter = sorted(list(set(query_subset_video_ids_filter)))
+
+    print("Transfer videos to images every one second using imageio")
+    videos_to_images(query_subset_video_ids_filter)
+
     print("Using detection models to generate new test sets")
     detectection()
     generation()
-    
+
     print("Fix rotating")
     fix_rotate()
 
@@ -250,10 +297,10 @@ def main():
 
     print("Ensemble")
     ensemble()
-    
+
     print("Normalize and Transfer hdf5 to npz")
     score_normalization()
     print("Total time = ", time.time()-begin)
 
 if __name__ == "__main__":
-    main()
+    main()(base)
